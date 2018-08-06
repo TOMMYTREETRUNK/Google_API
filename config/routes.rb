827fbe4dafc6e0781b95a3a19002f5bc04e9afd1
TOMMYTREETRUNK <tommyg1300@yahoo.com>
2018-08-06 08:21:07 -0400

    Google_API

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+  resources :destinations
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+
+# AIzaSyD_4J407Kh_Ad61dBwHIuIcovf7s-56PjI
